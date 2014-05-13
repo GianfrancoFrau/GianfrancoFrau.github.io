f5cbe9da92d649f2dd67b405ad9d9cad780ae9fe
gianfranco frau <gian.frau@hotmail.it>
2014-05-14 01:33:27 +0200

    assets added

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/**
+*	JQuery controller
+*/
+$( function() {
+	// Enables credits popover
+	$("#credits a").popover({
+		html: true,
+		placement: 'top',
+		container: 'body',
+		title: 'Credits',
+		content: 	'<ul id="credits-list">\n'+
+					'<li><a href="#">Github Pages</a></li>\n'+
+					'<li><a href="#">Jekyll</a></li>\n'+
+					'<li><a href="#">Bower</a></li>\n'+
+					'<li><a href="#">Bootstrap</a></li>\n'+
+					'<li><a href="#">JQuery</a></li>\n'+
+					'<li><a href="#">Ionicons</a></li>\n'+
+					'</ul>'
+	});
+});
