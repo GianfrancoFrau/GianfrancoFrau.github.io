3be8949cfabb0d4dc28475d44e90d49d9ae87fbd
gianfranco frau <gian.frau@hotmail.it>
2014-05-14 02:30:20 +0200

    fixing credits

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,12 +9,12 @@ $( function() {
 		container: 'body',
 		title: 'Credits',
 		content: 	'<ul id="credits-list">\n'+
-					'<li><a href="#" target="_blank">Github Pages</a></li>\n'+
-					'<li><a href="#" target="_blank">Jekyll</a></li>\n'+
-					'<li><a href="#" target="_blank">Bower</a></li>\n'+
-					'<li><a href="#" target="_blank">Bootstrap</a></li>\n'+
-					'<li><a href="#" target="_blank">JQuery</a></li>\n'+
-					'<li><a href="#" target="_blank">Ionicons</a></li>\n'+
+					'<li><a href="https://github.com/" target="_blank">Hosted on Github</a></li>\n'+
+					'<li><a href="http://jekyllrb.com/" target="_blank">Jekyll</a></li>\n'+
+					'<li><a href="http://bower.io/" target="_blank">Bower</a></li>\n'+
+					'<li><a href="http://getbootstrap.com/" target="_blank">Bootstrap</a></li>\n'+
+					'<li><a href="https://jquery.com/" target="_blank">JQuery</a></li>\n'+
+					'<li><a href="http://ionicons.com/" target="_blank">Ionicons</a></li>\n'+
 					'<li><a href="http://thepatternlibrary.com/" target="_blank">ThePatternLibrary</a></li>\n' +
 					'</ul>'
 	});
